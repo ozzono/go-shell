a63ac49e906801e5bb8119fefd54a4cfb4b22810
Hugo Virgilio <huvirgilio@gmail.com>
2020-10-07 15:47:17 -0300

    Initial commit

---
+++ go-shell.go
@@ -0,0 +1,19 @@
+package goshell
+
+import (
+	"log"
+	"os/exec"
+	"strings"
+)
+
+//Shell executes the given string as shell command
+func Shell(arg string) string {
+	log.Printf("shell: %v", arg)
+	args := strings.Split(arg, " ")
+	out, err := exec.Command(args[0], args[1:]...).CombinedOutput()
+	if err != nil {
+		log.Printf("Command: '%v';\nOutput: %v;\nError: %v\n", arg, string(out), err)
+		return err.Error()
+	}
+	return string(out)
+}
