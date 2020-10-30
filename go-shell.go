b59fb2c2e774717be8ea9624b0ca73638dc96a62
Hugo Virgilio <huvirgilio@gmail.com>
2020-10-29 23:42:42 -0300

    Removed changes to Cmd; added LooseCmd; added LooseCmd test case

--- go-shell.go
+++ go-shell.go
@@ -1,20 +1,49 @@
 package goshell
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
 
-// CMD basic structure with output and pid
-type CMD struct {
-	Output string
-	Pid    int
+// Cmd executes the given string as shell command
+//
+// Besided the PID as int, returns the output as string.
+func Cmd(arg string) (string, error) {
+	args := strings.Split(arg, " ")
+	out, err := exec.Command(args[0], args[1:]...).CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
 }
 
-//Cmd executes the given string as shell command
-func Cmd(arg string) (CMD, error) {
+// LooseCmd executes the given string as shell command
+// and send it to the background automatically
+//
+// Besided the PID as int, returns the output as string.
+func LooseCmd(arg string) (int, error) {
 	args := strings.Split(arg, " ")
-	cmd := exec.Command(args[0], args[1:]...)
-	out, err := cmd.CombinedOutput()
-	return CMD{Output: string(out), Pid: cmd.Process.Pid}, err
+	process, err := os.StartProcess(
+		args[0],
+		args,
+		&os.ProcAttr{
+			Dir: ".",
+			Env: os.Environ(),
+			Files: []*os.File{
+				os.Stdin,
+				nil,
+				nil,
+			},
+		})
+	if err != nil {
+		return 0, fmt.Errorf("os.StartProcess err: %v", err)
+	}
+	pid := process.Pid
+	err = process.Release()
+	if err != nil {
+		return 0, fmt.Errorf("process.Release err: %v", err)
+	}
+	return pid, nil
 }
--- go-shell_test.go
+++ go-shell_test.go
@@ -1,28 +1,71 @@
 package goshell
 
 import (
+	"fmt"
+	"math/rand"
+	"os"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestCMD(t *testing.T) {
-	cmd, err := Cmd("ls")
+	err := testCmd((t))
 	if err != nil {
-		t.Error(err)
+		t.Error((err))
 		return
 	}
-	cmdRows := []string{}
-	for _, item := range strings.Split(cleanString(cmd.Output), "\n") {
+
+	err = testLooseCmd((t))
+	if err != nil {
+		t.Error((err))
+		return
+	}
+}
+
+func testLooseCmd(t *testing.T) error {
+	sleep := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(999) + 5
+	t.Logf("starting testLooseCmd")
+	pid, err := LooseCmd(fmt.Sprintf("/bin/sleep %d", sleep))
+	if err != nil {
+		return fmt.Errorf("LooseCmd err: %v", err)
+	}
+	proc, err := os.FindProcess(pid)
+	if err != nil {
+		return fmt.Errorf("FindProcess err: %v", err)
+	}
+	t.Logf("pid: %d", pid)
+	err = proc.Kill()
+	if err != nil {
+		return fmt.Errorf("Kill err: %v", err)
+	}
+	t.Logf("successfully tested LooseCmd")
+	return nil
+}
+
+func testCmd(t *testing.T) error {
+	t.Logf("starting testCmd")
+	out, err := Cmd("ls")
+	if err != nil {
+		t.Error(err)
+		return err
+	}
+	outRows := []string{}
+	for _, item := range strings.Split(cleanString(string(out)), "\n") {
 		if len(item) > 0 {
-			cmdRows = append(cmdRows, item)
+			outRows = append(outRows, item)
 		}
 	}
 
-	// expects the existence of two files
-	if len(cmdRows) != 2 {
-		t.Errorf("More files than intended: %d", len(cmdRows))
-		return
+	// expects the existence of two visible files
+	// go-shell.go
+	// go-shell_test.go
+	if len(outRows) != 2 {
+		return fmt.Errorf("More files than intended: %d", len(outRows))
 	}
+
+	t.Logf("successfully tested Cmd")
+	return nil
 }
 
 func cleanString(input string) string {
